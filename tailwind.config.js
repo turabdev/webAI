dbfce0726d376368bcc57bad71040a7a6fef0c9a
turabdev <turabzaidi1971@gmail.com>
2024-07-06 21:39:40 +0500

    0

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jost: ["Jost", "sans-serif"],
+      },
+    },
+    plugins: [],
+  },
+};
